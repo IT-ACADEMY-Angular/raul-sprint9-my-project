0f5e805d25fb8e905adb8ca93e85a6130a3ef42d
rawl0408 <raulin4bcn@gmail.com>
2025-02-04 10:22:27 +0100

    Sprint 9 - creación estructura proyecto

---
+++ src/app/components/edit-worker/edit-worker.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-worker',
+  imports: [],
+  templateUrl: './edit-worker.component.html',
+  styleUrl: './edit-worker.component.css'
+})
+export class EditWorkerComponent {
+
+}
---
+++ src/app/components/pending-bookings/pending-bookings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pending-bookings',
+  imports: [],
+  templateUrl: './pending-bookings.component.html',
+  styleUrl: './pending-bookings.component.css'
+})
+export class PendingBookingsComponent {
+
+}
