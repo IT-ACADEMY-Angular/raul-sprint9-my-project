9af749a03a60e70d46e71997add67d11b54d8387
Rawl <raulin4bcn@gmail.com>
2025-02-08 16:27:19 +0100

    Sprint 9 - creada ventana de RESERVAR CITA, cambiaos insignificantes de datos en tas-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,11 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
 import { NewCompanyComponent } from './components/new-company/new-company.component';
 import { EditWorkerComponent } from './components/edit-worker/edit-worker.component';
+import { BookingComponent } from './components/booking/booking.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegisterComponent, LoginComponent, NavbarComponent, HomeComponent, ProfileComponent, EditProfileComponent, NewCompanyComponent, EditWorkerComponent],
+  imports: [RouterOutlet, RegisterComponent, LoginComponent, NavbarComponent, HomeComponent, ProfileComponent, EditProfileComponent, NewCompanyComponent, EditWorkerComponent, BookingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'booking-component',
-  imports: [],
+  imports: [CommonModule, FormsModule, MatDatepickerModule, MatInputModule, MatFormFieldModule, MatNativeDateModule],
   templateUrl: './booking.component.html',
-  styleUrl: './booking.component.css'
+  styleUrl: './booking.component.css',
+  encapsulation: ViewEncapsulation.None
+
 })
 export class BookingComponent {
+  breadcrumb: string = 'Reservar';
+  selectedWorker: string = '';
+  selectedService: string = '';
+  selectedSchedule: string = '';
+  selectedTime: string = '';
 
+  selectedDate: Date | null = new Date();
+
+  morningTimes: string[] = ['09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00'];
+  afternoonTimes: string[] = ['15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00'];
+
+  goBack() {
+    window.history.back();
+  }
+
+  selectWorker(name: string) {
+    this.selectedWorker = name;
+  }
+
+  selectService(service: string) {
+    this.selectedService = service;
+  }
+
+  selectSchedule(range: string) {
+    this.selectedSchedule = range;
+    this.selectedTime = '';
+  }
+
+  selectTime(time: string) {
+    this.selectedTime = time;
+  }
+
+  dateChanged(date: Date) {
+    this.selectedDate = date;
+    console.log('Fecha seleccionada:', date);
+  }
 }
