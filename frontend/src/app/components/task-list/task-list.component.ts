b4177d7b8c867967cfb1313e2bc63b1704eace40
Rawl <raulin4bcn@gmail.com>
2025-03-29 18:15:08 +0100

    added use of modal confirmation when deleting tasks/workers

--- frontend/src/app/components/task-list/task-list.component.ts
+++ frontend/src/app/components/task-list/task-list.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from '../../interfaces/task.interface';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalConfirmDialogComponent } from '../modal-confirm-dialog/modal-confirm-dialog.component';
 
 @Component({
   selector: 'task-list-component',
@@ -12,7 +14,21 @@ export class TaskListComponent {
   @Input() tasks: Task[] = [];
   @Output() removeTask = new EventEmitter<number>();
 
+  constructor(private dialog: MatDialog) { }
+
   onRemoveTask(index: number): void {
-    this.removeTask.emit(index);
+    const task = this.tasks[index];
+    const dialogRef = this.dialog.open(ModalConfirmDialogComponent, {
+      data: {
+        title: 'Eliminar tarea',
+        message: `¿Está seguro de que desea eliminar la tarea "${task.name}"?`
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(confirmed => {
+      if (confirmed) {
+        this.removeTask.emit(index);
+      }
+    });
   }
 }
--- frontend/src/app/components/worker-list/worker-list.component.ts
+++ frontend/src/app/components/worker-list/worker-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { WorkerData } from '../../models/worker.model';
 import { CommonModule } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalConfirmDialogComponent } from '../modal-confirm-dialog/modal-confirm-dialog.component';
 
 @Component({
   selector: 'worker-list-component',
@@ -13,9 +15,23 @@ export class WorkerListComponent {
   @Output() workersChange = new EventEmitter<WorkerData[]>();
   @Output() editWorker = new EventEmitter<{ worker: WorkerData; index: number }>();
 
+  constructor(private dialog: MatDialog) { }
+
   removeWorker(index: number): void {
-    this.workers.splice(index, 1);
-    this.workersChange.emit(this.workers);
+    const worker = this.workers[index];
+    const dialogRef = this.dialog.open(ModalConfirmDialogComponent, {
+      data: {
+        title: 'Eliminar trabajador',
+        message: `¿Está seguro de que desea eliminar al trabajador "${worker.name}"?`
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(confirmed => {
+      if (confirmed) {
+        this.workers.splice(index, 1);
+        this.workersChange.emit(this.workers);
+      }
+    });
   }
 
   onEditWorker(worker: WorkerData, index: number): void {
