578bf245ac6757933909c0ebc0a64884c60e6e48
Rawl <raulin4bcn@gmail.com>
2025-04-01 21:26:45 +0200

    fixed error (unauthorized) in login component when trying to login without mail/password + changed style and size from error messages in login/register components

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
@@ -16,12 +16,16 @@ export class LoginComponent {
   email: string = '';
   password: string = '';
   errorMessage: string = '';
-
   keepLoggedIn: boolean = false;
+  submitted: boolean = false;
 
   constructor(private authService: AuthService, private router: Router) { }
 
-  onLogin(): void {
+  onLogin(form: NgForm): void {
+    this.submitted = true;
+    if (form.invalid) {
+      return;
+    }
     this.authService.login(this.email, this.password).subscribe(
       (response: any) => {
         this.errorMessage = '';
