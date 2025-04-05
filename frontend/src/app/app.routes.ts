8bbe3b7e0c2293672f29a3546a35ad5dc7e7ff43
Rawl <raulin4bcn@gmail.com>
2025-04-05 17:19:38 +0200

    added lazyload in components loading

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -13,15 +13,94 @@ import { BookingsResultsComponent } from './components/bookings-results/bookings
 import { EditCompanyComponent } from './components/edit-company/edit-company.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, data: { hideProfileIcon: false, title: 'ZYTAPP' } },
-  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard], data: { hideProfileIcon: true } },
-  { path: 'edit-profile', component: EditProfileComponent, canActivate: [AuthGuard], data: { hideProfileIcon: true } },
-  { path: 'booking/:id', component: BookingComponent, data: { hideProfileIcon: true } },
-  { path: 'new-company', component: NewCompanyComponent, data: { hideProfileIcon: true } },
-  { path: 'pending-booking', component: PendingBookingsComponent, data: { hideProfileIcon: true } },
-  { path: 'login', component: LoginComponent, data: { hideProfileIcon: true } },
-  { path: 'register', component: RegisterComponent, data: { hideProfileIcon: true } },
-  { path: 'company-management', component: CompanyManagementComponent, data: { hideProfileIcon: true } },
-  { path: 'bookings-results', component: BookingsResultsComponent, data: { hideProfileIcon: true } },
-  { path: 'edit-company', component: EditCompanyComponent, data: { hideProfileIcon: true } },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/home/home.component').then(
+        (m) => m.HomeComponent
+      ),
+    data: { hideProfileIcon: false, title: 'ZYTAPP' },
+  },
+  {
+    path: 'profile',
+    loadComponent: () =>
+      import('./components/profile/profile.component').then(
+        (m) => m.ProfileComponent
+      ),
+    canActivate: [AuthGuard],
+    data: { hideProfileIcon: true },
+  },
+  {
+    path: 'edit-profile',
+    loadComponent: () =>
+      import('./components/edit-profile/edit-profile.component').then(
+        (m) => m.EditProfileComponent
+      ),
+    canActivate: [AuthGuard],
+    data: { hideProfileIcon: true },
+  },
+  {
+    path: 'booking/:id',
+    loadComponent: () =>
+      import('./components/booking/booking.component').then(
+        (m) => m.BookingComponent
+      ),
+    data: { hideProfileIcon: true },
+  },
+  {
+    path: 'new-company',
+    loadComponent: () =>
+      import('./components/new-company/new-company.component').then(
+        (m) => m.NewCompanyComponent
+      ),
+    data: { hideProfileIcon: true },
+  },
+  {
+    path: 'pending-booking',
+    loadComponent: () =>
+      import('./components/pending-bookings/pending-bookings.component').then(
+        (m) => m.PendingBookingsComponent
+      ),
+    data: { hideProfileIcon: true },
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./components/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
+    data: { hideProfileIcon: true },
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./components/register/register.component').then(
+        (m) => m.RegisterComponent
+      ),
+    data: { hideProfileIcon: true },
+  },
+  {
+    path: 'company-management',
+    loadComponent: () =>
+      import('./components/company-management/company-management.component').then(
+        (m) => m.CompanyManagementComponent
+      ),
+    data: { hideProfileIcon: true },
+  },
+  {
+    path: 'bookings-results',
+    loadComponent: () =>
+      import('./components/bookings-results/bookings-results.component').then(
+        (m) => m.BookingsResultsComponent
+      ),
+    data: { hideProfileIcon: true },
+  },
+  {
+    path: 'edit-company',
+    loadComponent: () =>
+      import('./components/edit-company/edit-company.component').then(
+        (m) => m.EditCompanyComponent
+      ),
+    data: { hideProfileIcon: true },
+  },
 ];
