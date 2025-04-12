dfd7add009903439c1deb06ccaf887a4acdd9a82
Rawl <raulin4bcn@gmail.com>
2025-04-12 17:31:26 +0200

    added apibackend in production environment + installed missing dependecies

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.tu-dominio.com',
+  apiUrl: 'mysql://root:SCedQgZQkOUYkwzSeWIYkUNTRMvxSufX@mysql.railway.internal:3306/railway',
 };
