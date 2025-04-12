df8753ea88849b7dfea987961123d13d9108ba3c
Rawl <raulin4bcn@gmail.com>
2025-04-12 13:59:48 +0200

    added log in main.ts for check errors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,8 @@ if (!globalThis.crypto) {
   globalThis.crypto = webcrypto as unknown as Crypto;
 }
 
+console.log('ALL ENV:', process.env);
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
