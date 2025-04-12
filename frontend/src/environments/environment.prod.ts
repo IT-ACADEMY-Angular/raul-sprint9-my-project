5be0c449028e85595a62c2ce756c5382796b47d3
Rawl <raulin4bcn@gmail.com>
2025-04-12 18:54:39 +0200

    changed apiUrl backend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'mysql://root:SCedQgZQkOUYkwzSeWIYkUNTRMvxSufX@mysql.railway.internal:3306/railway',
+  apiUrl: 'https://raul-sprint9-my-project-production.up.railway.app',
 };
